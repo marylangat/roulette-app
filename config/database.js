87ef9bbd0f6e2570dee5caa111fdb1632cc4217b
Marymagdalene <chemtai.langat92@gmail.com>
2021-10-25 13:39:04 -0400

    upload all files

---
+++ config/database.js
@@ -0,0 +1,11 @@
+
+
+
+// config/database.js
+const username  = 'Mary'
+const password  = 'BZxn35BzacSsb.!'
+const dbName    = 'roulette'
+
+module.exports = {
+    'url' : `mongodb+srv://${username}:${password}@taskmanagercluster.aqyy0.mongodb.net/${dbName}?retryWrites=true&w=majority`};
+//mongodb+srv://Mary:<password>@taskmanagercluster.aqyy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
